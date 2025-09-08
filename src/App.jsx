e1d792d1c015141544a5bc9d90d665586a0a41a0
Андрей <Winterles@yandex.ru>
2025-09-08 15:36:52 +0300

    Подготовка проекта

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+  text-align: center;
+  font-size: 40px;
+`;
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Div></Div>
+    </>
+  );
+}
+
+export default App;
